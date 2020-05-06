8030051a47db261e96d7a7c55433ff760c7d1adb
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-06 20:45:06 +0100

    inital commit creates vue file strucuture

---
+++ client/src/App.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div class="app">
+
+  </div>
+</template>
+
+<script>
+import { eventBus } from '@/main.js'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
