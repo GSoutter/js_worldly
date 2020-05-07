5441604ba804caf2bfcbce0162c9bc6253f166bd
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-08 00:28:38 +0100

    adds working find country game.

--- client/src/components/OutlineMap.vue
+++ client/src/components/OutlineMap.vue
@@ -1,7 +1,14 @@
 <template lang="html">
-  <div class="" id="chartdiv">
-    <h1>Map div</h1>
+  <div class="">
+    <button v-on:click="randomArrayItem(polygonData)">Generate another country</button>
+    <h4 v-if="answerCountry">Find {{this.answerCountry.name}}</h4>
+    <h5 v-if="oldEv">you have selected {{this.oldEv.dataItem.dataContext.name}}</h5>
+    <button  v-if="correct" v-on:click="resetAnswer">Well done. Try agin?</button>
+
+    <div class="" id="chartdiv">
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -19,8 +26,11 @@ export default {
   name: 'outline-map',
   data() {
     return {
-      selectedCountry: 'france',
+      selectedCountry: [],
       map: null,
+      polygonData: [],
+      oldEv: null,
+      answerCountry: null,
 
     }
   },
@@ -29,22 +39,64 @@ export default {
     this.map.geodata = am4geodata_worldLow
     this.map.projection = new am4maps.projections.Miller()
     var polygonSeries = this.map.series.push(new am4maps.MapPolygonSeries())
-    polygonSeries.useGeodata = true
-    var polygonTemplate = polygonSeries.mapPolygons.template;
-    polygonTemplate.events.on("hit", (ev) => {
-      // zoom to an object
-      // ev.target.series.chart.zoomToMapObject(ev.target);
 
-      // get object info
-      console.log(ev.target.dataItem); // dsfa
-      this.selectedCountry = ev.target.dataItem.dataContext.name;
-      console.log(this.selectedCountry);
+    polygonSeries._data = this.polygonData
+
+    //excludes antartica
+    polygonSeries.exclude = ["AQ"]
+    polygonSeries.useGeodata = true
+
+    var polygonTemplate = polygonSeries.mapPolygons.template;
+
+    var hs = polygonTemplate.states.create("hover");
+    hs.properties.fill = am4core.color("#367B25");
+
+    var activeState = polygonTemplate.states.create("active");
+    activeState.properties.fill = this.map.colors.getIndex(2);
+
+
+    // select listener, and highlight
+    polygonTemplate.events.on("hit", (ev) => {
+      if (this.oldEv) {
+        this.oldEv.isActive = false
+      }
+      ev.target.isActive = !ev.target.isActive;
+      console.log(ev.target.dataItem.dataContext.id);
+
+      this.oldEv = ev.target;
     });
+
+
   },
   beforeDestroy() {
     if (this.map) {
       this.map.dispose()
     }
+  },
+  computed: {
+    correct() {
+      if (!this.answerCountry || !this.oldEv) {
+        return false
+      }
+      if (this.answerCountry.name === this.oldEv.dataItem.dataContext.name) {
+        return true
+      } else {
+        return false
+      }
+    }
+  },
+  methods: {
+    randomArrayItem: function(array){
+      console.log('buttonClicked');
+      const max = array.length;
+      const randomIndex = Math.floor(Math.random() * Math.floor(max));
+      console.log(array[randomIndex]);
+      this.answerCountry = array[randomIndex];
+    },
+    resetAnswer: function(){
+      this.randomArrayItem(this.polygonData)
+    },
+
   }
 }
 </script>
