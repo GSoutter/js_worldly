9745f42c4903886ce73a0db16876bb9ad9b5bd5f
Kira Marston <kiramarston@gmail.com>
2020-05-09 15:55:10 +0100

    Multiple choice method working (kinda)

--- client/src/components/CapitalsQuiz.vue
+++ client/src/components/CapitalsQuiz.vue
@@ -1,21 +1,19 @@
 <template lang="html">
 
 <div>
-<!-- <li v-for="country in countries">{{country.name}}  {{country.capital}}</li> -->
 <button @click="getRandomCountry" type="button" v-if="randomCountry === null">Click to begin!</button>
 
   <div v-if="randomCountry">
+    <button @click="getPossibleAnswers">Do it</button>
   <h2>What is the capital of {{this.randomCountry.name}}?</h2>
   <input v-model="userGuess" v-on:keyup.enter="checkGuess">
-  <!-- <input v-model="userGuess">
-  <button v-on:click="checkGuess">Guess</button> -->
   <p v-if="this.userGuessResult === false">Bad luck, try again!
   <br>
   <button @click="getRandomCountry" type="button">I want a new country please!</button></p>
 
     <div v-if="this.userGuessResult === true">
       <p>Well done!</p>
-      <p><img :src="this.randomCountry.flag" width="150" height="100"></p>
+      <p><img :src="this.randomCountry.flag" width="150"></p>
       <p>The capital of {{this.randomCountry.name}} is {{this.randomCountry.capital}}!</p>
       <p>People from {{this.randomCountry.name}} are called {{this.randomCountry.demonym}}.</p>
       <p>{{this.randomCountry.name}} is part of {{this.randomCountry.region}}.</p>
@@ -35,7 +33,8 @@ export default {
     return {
       randomCountry: null,
       userGuess: "",
-      userGuessResult: null
+      userGuessResult: null,
+      possibleAnswers: []
     }
   },
   props: ['countries'],
@@ -55,10 +54,39 @@ export default {
       }
         this.userGuess = ""
       return this.userGuessResult;
-      }
+    },
+    getPossibleAnswers(){
+      this.possibleAnswers.push(this.randomCountry.capital);
+      const max = this.countries.length;
+      let randomIndex1 = Math.floor(Math.random() * Math.floor(max));
+      let wrongCountry1 = this.countries[randomIndex1];
+      let wrongAnswer1 = wrongCountry1.capital;
+      this.possibleAnswers.push(wrongAnswer1);
+      let randomIndex2 = Math.floor(Math.random() * Math.floor(max));
+      let wrongCountry2 = this.countries[randomIndex2];
+      let wrongAnswer2 = wrongCountry2.capital;
+      this.possibleAnswers.push(wrongAnswer2);
+      let randomIndex3 = Math.floor(Math.random() * Math.floor(max));
+      let wrongCountry3 = this.countries[randomIndex3];
+      let wrongAnswer3 = wrongCountry3.capital;
+      this.possibleAnswers.push(wrongAnswer3);
+      return this.possibleAnswers
+      console.log(this.possibleAnswers);
     }
   }
+}
 </script>
 
 <style lang="css" scoped>
 </style>
+
+<!-- checkGuess(){
+  const correctCapital = this.randomCountry.capital;
+  if (this.userGuess === correctCapital){
+      this.userGuessResult = true;
+  }else{
+      this.userGuessResult = false;
+  }
+    this.userGuess = ""
+  return this.userGuessResult;
+  } -->
