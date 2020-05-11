8a60834443cf236005b9d9cf651c424e4e70dab2
Mark Nicol <marknicol1991@live.co.uk>
2020-05-11 14:52:56 +0100

    removes redundent code from capitals quiz.

--- client/src/components/CapitalsQuiz.vue
+++ client/src/components/CapitalsQuiz.vue
@@ -31,6 +31,7 @@
 </template>
 
 <script>
+
 import {eventBus} from '@/main.js'
 export default {
   data(){
@@ -40,45 +41,10 @@ export default {
   },
   props: ['answerCountry', 'possibleAnswers'],
   methods: {
-    // getRandomCountry(){
-    //   this.userGuessResult = null;
-    //   const max = this.countries.length;
-    //   let randomIndex = Math.floor(Math.random() * Math.floor(max));
-    //   while (!this.countries[randomIndex].capital){
-    //     randomIndex = Math.floor(Math.random() * Math.floor(max));
-    //   }
-    //   return this.countries[randomIndex];
-
-      // getRandomCountry() will calculate a random index and then return a
-      // country from the Countries array at that index if that country has a
-      // capital. It will also reset the user's guess each time it is called.
-
-    // },
-
     generateQuestion(){
       this.userGuessResult = null;
       eventBus.$emit('play-quiz');
     },
-
-    // getPossibleAnswers() uses getRandomCountry() to create a new array of
-    // multiple choice options (this.possibleAnswers). It also sets the correct
-    // answer country to a random country. The for loop generated answers for
-    // the multiple choice options, providing that they do not already exist
-    // within the array. These are then shuffled.
-
-    // shuffleArray(){
-    //   for (let i = this.possibleAnswers.length - 1; i > 0; i --){
-    //     const newPos = Math.floor(Math.random() * (i + 1))
-    //     const temp = this.possibleAnswers[i];
-    //     this.possibleAnswers[i] = this.possibleAnswers[newPos];
-    //     this.possibleAnswers[newPos] = temp;
-    //   }
-    //   return this.possibleAnswers
-    // },
-
-    // shuffleArray() randomises the indexes of the array, so that the correct
-    // answer isn't always at the (default) first index.
-
     checkGuess(guess){
       if (guess === this.answerCountry){
           this.userGuessResult = true;
@@ -86,16 +52,10 @@ export default {
           this.userGuessResult = false;
       }
     },
-
-    // checkGuess() will compare the correct country's capital to the user's
-    // guess. The result is used to conditionally render feedback to the user
-    // in the html.
   }
 }
 
 </script>
-
-
 
 <style lang="css" scoped>
 </style>
