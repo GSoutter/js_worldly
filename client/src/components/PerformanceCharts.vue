430540fb848cfb0ed39c60770dd6ff33248b0cf5
Gordon Soutter <gordon.soutter@outlook.com>
2020-05-12 19:13:07 +0100

    adds flag chart display

--- client/src/components/PerformanceCharts.vue
+++ client/src/components/PerformanceCharts.vue
@@ -13,23 +13,30 @@
 
       <div v-if="chartSelect === 'table'">
         <h3>Accuracy Table</h3>
-        <map-chart v-if="mapMostInaccurate" :mapMostAccurate="mapMostAccurate" :mapMostInaccurate="mapMostInaccurate" />
+        <map-chart v-if="mapMostInaccurate"
+        :mapMostAccurate="mapMostAccurate"
+        :mapMostInaccurate="mapMostInaccurate" />
       </div>
 
       <div v-if="chartSelect === 'heatmap'">
         <h3>Accuracy Heatmap</h3>
-        <heat-map v-if="mapPerformanceAccuracy" :mapPerformanceAccuracy="mapPerformanceAccuracyOnly" />
+        <heat-map v-if="mapPerformanceAccuracy"
+        :mapPerformanceAccuracy="mapPerformanceAccuracyOnly" />
       </div>
     </div>
 
     <div v-if="selectedElement === 'capitalQuiz'">
       <h3>Capital Quiz: Accuracy Table</h3>
-      <!-- <capital-chart v-if="mapMostInaccurate" :mapMostAccurate="mapMostAccurate" :mapMostInaccurate="mapMostInaccurate" /> -->
+      <!-- <capital-chart v-if="capitalMostInaccurate"
+      :mostAccurate="capitalMostAccurate"
+      :mostInaccurate="capitalMostInaccurate" /> -->
     </div>
 
     <div v-if="selectedElement === 'flagQuiz'">
       <h3>Flag Quiz: Accuracy Table</h3>
-      <!-- <capital-chart v-if="mapMostInaccurate" :mapMostAccurate="mapMostAccurate" :mapMostInaccurate="mapMostInaccurate" /> -->
+      <flag-chart v-if="flagMostInaccurate"
+      :mostAccurate="flagMostAccurate"
+      :mostInaccurate="flagMostInaccurate" />
     </div>
 
   </div>
@@ -37,6 +44,8 @@
 
 <script>
 import MapChart from '@/components/charts/MapChart.vue'
+import FlagChart from '@/components/charts/FlagChart.vue'
+import CapitalChart from '@/components/charts/CapitalChart.vue'
 import HeatMap from '@/components/charts/HeatMap.vue'
 
 export default {
@@ -50,17 +59,45 @@ export default {
       mapPerformanceAccuracyOnly: null,
       mapMostAccurate: null,
       mapMostInaccurate: null,
+      capitalPerformanceAccuracy: null,
+      capitalMostAccurate: null,
+      capitalMostInaccurate: null,
+      flagPerformanceAccuracy: null,
+      flagMostAccurate: null,
+      flagMostInaccurate: null,
 
     }
   },
   mounted() {
     // calculates accuracy of guesses
-    this.mapPerformanceAccuracy = this.getAccuracy(this.mapPerformance, 'correct_answers','wrong_answers', 'map_accuracy' )
-    this.mapPerformanceAccuracyOnly = this.getAccuracyOnly(this.mapPerformance, 'correct_answers','wrong_answers', 'map_accuracy' )
+    this.mapPerformanceAccuracy = this.getAccuracy(this.mapPerformance,
+      'correct_answers','wrong_answers', 'map_accuracy' )
+    this.mapPerformanceAccuracyOnly = this.getAccuracyOnly(this.mapPerformance,
+      'correct_answers','wrong_answers', 'map_accuracy' )
     // gets 10 countries where the user has performed the best
-    this.mapMostAccurate = this.getTopTenAccurate(this.mapPerformanceAccuracy, 'correct_answers','wrong_answers', 'map_accuracy', true )
+    this.mapMostAccurate = this.getTopTenAccurate(this.mapPerformanceAccuracy,
+      'correct_answers','wrong_answers', 'map_accuracy', true )
     // gets 10 countries where the user has performed the worst
-    this.mapMostInaccurate = this.getTopTenAccurate(this.mapPerformanceAccuracy, 'correct_answers','wrong_answers', 'map_accuracy', false )
+    this.mapMostInaccurate = this.getTopTenAccurate(this.mapPerformanceAccuracy,
+      'correct_answers','wrong_answers', 'map_accuracy', false )
+
+    this.capitalPerformanceAccuracy = this.getAccuracy(this.countries,
+      'capital_correct_ans','capital_wrong_ans', 'capital_accuracy' )
+    // gets 10 countries where the user has performed the best
+    this.mapMostAccurate = this.getTopTenAccurate(this.capitalPerformanceAccuracy,
+      'capital_correct_ans','capital_wrong_ans', 'capital_accuracy', true )
+    // gets 10 countries where the user has performed the worst
+    this.mapMostInaccurate = this.getTopTenAccurate(this.capitalPerformanceAccuracy,
+      'capital_correct_ans','capital_wrong_ans', 'capital_accuracy', false )
+
+    this.flagPerformanceAccuracy = this.getAccuracy(this.countries,
+      'flag_correct_ans','flag_wrong_ans', 'flag_accuracy' )
+    // gets 10 countries where the user has performed the best
+    this.flagMostAccurate = this.getTopTenAccurate(this.capitalPerformanceAccuracy,
+      'flag_correct_ans','flag_wrong_ans', 'flag_accuracy', true )
+    // gets 10 countries where the user has performed the worst
+    this.flagMostInaccurate = this.getTopTenAccurate(this.capitalPerformanceAccuracy,
+      'flag_correct_ans','flag_wrong_ans', 'flag_accuracy', false )
 
 
   }, // mounted end
@@ -106,7 +143,7 @@ export default {
       // loops through sorted array. Checks if there are greater than 20 answers, pushes to array. Once there are 10 items it returns.
       for (let country of sortedArray) {
         let totalAnswers = country[rightKey] + country[wrongKey]
-        if ((totalAnswers > 20)) {
+        if ((totalAnswers > 10)) {
           accuracyArray.push(country)
         }
         if (accuracyArray.length >= 10) {
@@ -119,7 +156,9 @@ export default {
   },
   components: {
     'map-chart': MapChart,
-    'heat-map': HeatMap
+    'flag-chart': FlagChart,
+    'capital-chart': CapitalChart,
+    'heat-map': HeatMap,
   }
 }
 </script>
