4388d748720afb54c590b70cce64731ace1162a7
Gordon Soutter <gordon.soutter@outlook.com>
2020-08-04 19:24:52 +0100

    changes changes url pathing in MapCountriesService to resolve get error

--- client/src/services/MapCountriesService.js
+++ client/src/services/MapCountriesService.js
@@ -1,4 +1,4 @@
-const baseURL = 'api/amMapPerformance'
+const baseURL = 'api/amMapPerformance/'
 
 export default {
   getCountries(){
@@ -10,7 +10,7 @@ export default {
     return fetch(baseURL, {
       method: "POST",
       body: JSON.stringify(payload),
-      headers: {'Content-Type': '/application/json'}
+      headers: {'Content-Type': 'application/json'}
     })
     .then(res => res.json())
   },
@@ -18,7 +18,7 @@ export default {
     return fetch(baseURL + id, {
       method: "PUT",
       body: JSON.stringify(payload),
-      headers: {'Content-Type': '/application/json'}
+      headers: {'Content-Type': 'application/json'}
     })
     .then(res => res.json())
 
